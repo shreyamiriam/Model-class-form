ecf0c455f9fa0fd4e349f6a448d880d7f1948adc
shreya miriam <shreyamiriamroy@gmail.com>
2024-12-05 22:14:27 +0530

    completed

--- modelformapp/admin.py
+++ modelformapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from . models import Customer
 # Register your models here.
+class CustomerAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Customer,CustomerAdmin)
